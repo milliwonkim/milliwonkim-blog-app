721fc3a55e989ba5c7531df6f00ad9bcb76a02b8
milliwonkim <rldnjs9347@gmail.com>
2023-06-27 02:31:24 +0900

    notion blog start

--- next.config.js
+++ next.config.js
@@ -1,33 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-})
-
-const debug = process.env.NODE_ENV !== 'production'
-const repository = 'milliwonkim-blog'
-
-module.exports = {
-  ...withBundleAnalyzer({
-    staticPageGenerationTimeout: 300,
-    images: {
-      domains: [
-        'www.notion.so',
-        'notion.so',
-        'images.unsplash.com',
-        'pbs.twimg.com',
-        'abs.twimg.com',
-        's3.us-west-2.amazonaws.com',
-        'transitivebullsh.it'
-      ],
-      formats: ['image/avif', 'image/webp'],
-      dangerouslyAllowSVG: true,
-      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-    }
-  }),
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  assetPrefix: './',
   images: {
     unoptimized: true
-  },
-  reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : '', // production 일때 prefix 경로
-  trailingSlash: true // 빌드 시 폴더 구조 그대로 생성하도록
+  }
 }
+
+module.exports = nextConfig
