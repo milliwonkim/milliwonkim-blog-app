a75b39ebce9f75677799d732b988598fbb80e17e
milliwonkim <rldnjs9347@gmail.com>
2023-06-27 02:06:57 +0900

    notion blog start

--- next.config.js
+++ next.config.js
@@ -3,20 +3,31 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-module.exports = withBundleAnalyzer({
-  staticPageGenerationTimeout: 300,
+const debug = process.env.NODE_ENV !== 'production'
+const repository = 'milliwonkim-blog'
+
+module.exports = {
+  ...withBundleAnalyzer({
+    staticPageGenerationTimeout: 300,
+    images: {
+      domains: [
+        'www.notion.so',
+        'notion.so',
+        'images.unsplash.com',
+        'pbs.twimg.com',
+        'abs.twimg.com',
+        's3.us-west-2.amazonaws.com',
+        'transitivebullsh.it'
+      ],
+      formats: ['image/avif', 'image/webp'],
+      dangerouslyAllowSVG: true,
+      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+    }
+  }),
   images: {
-    domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      'pbs.twimg.com',
-      'abs.twimg.com',
-      's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it'
-    ],
-    formats: ['image/avif', 'image/webp'],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-  }
-})
+    unoptimized: true
+  },
+  reactStrictMode: true,
+  assetPrefix: !debug ? `/${repository}/` : '', // production 일때 prefix 경로
+  trailingSlash: true // 빌드 시 폴더 구조 그대로 생성하도록
+}
