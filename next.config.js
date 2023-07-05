b8f2b854dfd547f746a1d604794b33da894f9d0a
milliwonkim <rldnjs9347@gmail.com>
2023-07-05 21:34:40 +0900

    notion render init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: './',
+  assetPrefix:
+    process.env.NODE_ENV === 'production' ? '/milliwonkim-blog-app/' : './',
   images: {
     unoptimized: true
   }
